756fc27ed6ccf10d3c4e6514b3ec266b9892148f
Prajyot <prajyotraut.dev@gmail.com>
2025-03-30 15:43:07 +0530

    removed center location and ai assitant

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,7 @@
-
 import Layout from "@/components/layout/Layout";
 import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
 import SurvivorHighlights from "@/components/home/SurvivorHighlights";
-import CareLocator from "@/components/home/CareLocator";
-import AIAssistant from "@/components/home/AIAssistant";
 import CallToAction from "@/components/home/CallToAction";
 
 const Index = () => {
@@ -13,8 +10,6 @@ const Index = () => {
       <Hero />
       <Features />
       <SurvivorHighlights />
-      <CareLocator />
-      <AIAssistant />
       <CallToAction />
     </Layout>
   );
